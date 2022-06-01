3b239e97a5cbd5017501c9392745171e1d9cb7ef
nurulkairiza <nurulkhairiza@gmail.com>
2022-06-02 02:28:14 +0700

    change debug to True

--- cogserv/settings.py
+++ cogserv/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-je&vyazg$%p1zg#pa7e!q&v9*)l17=81f2v12l008k$wa3q1=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'ocrify.azurewebsites.net' 
